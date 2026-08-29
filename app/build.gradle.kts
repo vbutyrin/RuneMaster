e87ba1d221407080d8317cac9984cd120e4df9e5
vbutyrin <34686764+vbutyrin@users.noreply.github.com>
2026-08-29 17:41:25 +0000

    Force Java Kotlin 17 toolchain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,10 +21,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         compose = true
     }
@@ -36,11 +32,19 @@ android {
     }
 }
 
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.compose.material3:material3")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
